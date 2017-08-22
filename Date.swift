2908591cbe47c29db2d31b2465c487a17e89870f
George Kye <qwstnz@gmail.com>
2017-08-22 12:40:43 -0400

    Create Date.swift

---
+++ Date.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+//Date
+extension Date {
+
+    func isBetween(_ beginDate: Date, and endDate: Date) -> Bool {
+        if self.compare(beginDate) == .orderedAscending {
+            return false
+        }
+
+        if self.compare(endDate) == .orderedDescending {
+            return false
+        }
+
+        return true
+    }
+
+    func dayAfter() -> Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: self)!
+    }
+
+    func futureDates() -> [Date] {
+        var dayArray = [self]
+
+        for i in 0 ..< 30 {
+            dayArray.append(dayArray[i].dayAfter())
+        }
+
+        return dayArray
+    }
+}
