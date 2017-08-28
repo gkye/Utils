7a0c1c27e1656bd43915aa0698497c47ea428791
George Kye <qwstnz@gmail.com>
2017-08-28 12:04:45 -0400

    Update UIView.swift

--- UIKit/UIView.swift
+++ UIKit/UIView.swift
@@ -24,4 +24,17 @@ extension UIView {
     public func parentCollectionViewCell () -> UICollectionViewCell? {
         return parentViewOfType(type: UICollectionViewCell.self)
     }
+    
+    public func snapshotImage() -> UIImage?{
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0.0)
+        if (self.responds(to: #selector(UIView.drawHierarchy(in:afterScreenUpdates:)))){
+            self.drawHierarchy(in: bounds, afterScreenUpdates: false)
+        }else{
+            self.layer.render(in: UIGraphicsGetCurrentContext()!)
+        }
+        let snapshotImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return snapshotImage
+    }
+    
 }
