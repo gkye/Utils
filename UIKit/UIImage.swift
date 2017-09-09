7ce02b6f6c8060ad3a09ead6e7de9e370190bb4e
George Kye <qwstnz@gmail.com>
2017-09-09 09:28:51 -0400

    Update UIImage.swift

--- UIKit/UIImage.swift
+++ UIKit/UIImage.swift
@@ -1,12 +1,45 @@
+//
+//  UIImage.swift
+//  Athena
+//
+//  Created by George Kye on 2017-09-09.
+//  Copyright © 2017 George Kye. All rights reserved.
+//
+
+import Foundation
 import UIKit
 
+enum ImageSize {
+
+	case small
+	case medium
+	case large
+	case custom(width: CGFloat, height: CGFloat)
+
+	func getSize() -> CGSize{
+		switch self {
+		case .small:
+			return CGSize(width: 150, height: 150)
+		case .medium:
+			return CGSize(width: 250, height: 250)
+		case .large:
+			return CGSize(width: 400, height: 400)
+		case .custom(let width, let height):
+			return CGSize(width: width, height: height)
+		}
+	}
+	
+}
+
 extension UIImage {
-    public func resize (size: CGSize) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
-        draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        let imageResized = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return imageResized!
-    }
+
+	func resize(size: ImageSize = .medium) -> UIImage {
+		let imgSize = size.getSize()
+		UIGraphicsBeginImageContextWithOptions(imgSize, false, 1.0)
+		draw(in: CGRect(origin: CGPoint(x: 0, y: 0), size: imgSize))
+		let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+
+		return resizedImage!
+	}
 }
