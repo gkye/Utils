48851fa46944fba096eb06ea5e133961a19de7d6
George Kye <qwstnz@gmail.com>
2018-04-15 18:51:56 -0400

    Apply sketch like shadow

--- UIKit/UIView.swift
+++ UIKit/UIView.swift
@@ -37,4 +37,30 @@ extension UIView {
         return snapshotImage
     }
     
+    /**
+        Apply shadow with sketch like properties
+        When using a non-zero spread, it hardcodes a path based on the bounds of the CALayer. 
+        If the layer's bounds ever change, you'd want to call the applySketchShadow() method again.
+        https://stackoverflow.com/a/48489506/5222077
+    **/
+    func applySketchShadow(
+    color: UIColor = .black,
+    alpha: Float = 0.5,
+    x: CGFloat = 0,
+    y: CGFloat = 2,
+    blur: CGFloat = 4,
+    spread: CGFloat = 0)
+  {
+    shadowColor = color.cgColor
+    shadowOpacity = alpha
+    shadowOffset = CGSize(width: x, height: y)
+    shadowRadius = blur / 2.0
+    if spread == 0 {
+      shadowPath = nil
+    } else {
+      let dx = -spread
+      let rect = bounds.insetBy(dx: dx, dy: dx)
+      shadowPath = UIBezierPath(rect: rect).cgPath
+    }
+  }
 }
